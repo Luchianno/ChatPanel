31a91b57562ef6c49deb8236e4644da318427102
Luka Tsulaia <lucho.tsulaia@yahoo.com>
2020-09-22 18:12:35 +0300

    init

---
+++ Assets/ChatPanel/Scripts/MessageBubble.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MessageBubble : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI text;
+    [SerializeField]
+    Image background;
+    [SerializeField]
+    Image arrow;
+
+    public Color BubbleColor { get { return background.color; } set { background.color = value; } }
+    public string Text { get { return text.text; } set { text.text= value; } }
+}
---
+++ Assets/ChatPanel/Scripts/MessageListManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MessageListManager : MonoBehaviour
+{
+    public GameObject BotMessageTemplate;
+    public GameObject UserMessageTemplate;
+    public RectTransform Parent;
+
+    public void AddReceivedMessage(string message)
+    {
+        AddMessage(BotMessageTemplate, message);
+    }
+
+    public void AddUserMessage(string message)
+    {
+        AddMessage(UserMessageTemplate, message);
+    }
+
+    void AddMessage(GameObject prefab, string message)
+    {
+        var temp = Instantiate(prefab, Parent);
+
+        var bubble = temp.GetComponent<MessageBubble>();
+
+        bubble.Text = message;
+
+    }
+
+
+
+}
